45f2c6a4ed0b5fa6bbe7f98912f552c3be42ceb0
app-demo <115422493+app-demo@users.noreply.github.com>
2022-10-09 22:06:05 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 if(navigator.serviceWorker){
     console.log("Service Worker is supported");
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('../sw_cache.js')
+        navigator.serviceWorker.register('sw_cache.js')
         .then(reg => console.log("Service Worker is registered"))
         .catch(err => console.log(err));
     }); 
